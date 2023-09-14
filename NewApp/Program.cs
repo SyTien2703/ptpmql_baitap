97a9ab90673f3a91990e83de577fea9e3073322c
SyTien270302 <2021050637@student.humg.edu.vn>
2023-09-14 23:04:17 +0700

    CreateProject

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// in ra màn hình "Hello, World"dot
+Console.Write (6);
+// in ra màn hình:6
+int a = 9;
+Console.WriteLine("a =" + a);
+//in ra màn hình a = 9
+
+int a = 9;
+int b = 3;
+Console.WriteLine ("{0} + {1} = {2}",a,b, a+b);
+// in ra màn hình: a + b = 12
