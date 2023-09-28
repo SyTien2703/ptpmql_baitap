4a52a9da133ccb3cfd8ad01cfff081dba8b317e7
SyTien270302 <2021050637@student.humg.edu.vn>
2023-09-28 14:16:16 +0700

    update code

---
+++ NewApp/NewApp/BaiTapC#/Models/Person.cs
@@ -0,0 +1,10 @@
+using NEWAPP.Models;
+
+ Person ps = new Person();
+ System.Console.WriteLine("Du lieu nhap vao la: ");
+ps.Nhapthongtin();
+ps.Hienthi();
+
+Student std = new Student();
+std.NhapThongTin();
+std.HienThi();
