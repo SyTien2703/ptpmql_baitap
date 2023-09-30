5983d812a12b04f71d4b2348a7aa4573e5fea9eb
SyTien270302 <2021050637@student.humg.edu.vn>
2023-09-30 23:32:11 +0700

    Upload Code

--- Models/Fruit.cs
+++ Models/Fruit.cs
@@ -1,15 +1,18 @@
-namespace NewApp.Models
- {
- public class Fruit {
-        public string FruitName {get;set;}
-        public string Information {get; set;}
-       
-
-        // phuong thuc chua tham so-Fruit
-        public int ThanhTien ( int CanNang)
-        {
-            int TTien = CanNang *2;
-            return TTien;
-        }
- }
- }
+using NewApp.Models;
+public class Fruit
+// nguyễn sỹ tiến - 2021050637
+{
+    private static void main(string[] args)
+    {
+        //khoitaodoituong
+        Fruit fr1 = new Fruit();
+        Fruit fr2 = new Fruit();
+        //gangiatrithuoctinh
+        Fruit1.Name = "Apple";
+        Fruit2.NguonGoc = "Korea";
+        //goi pt hien thi thong tin
+        fr1.Display();
+        fr2.Display();
+    }
+}
+     
