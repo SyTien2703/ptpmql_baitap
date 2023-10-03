ebc0a2e34fb927b6a06c2e6b6fb425d20add45ca
SyTien270302 <2021050637@student.humg.edu.vn>
2023-10-03 23:03:12 +0700

    Xu ly ngoai le try ... catch

--- Models/Person.cs
+++ Models/Person.cs
@@ -8,7 +8,7 @@ namespace NewApp.Models
        public string PersonID { get; set; }
        public string FullName { get; set; }
        public int Age { get; set; }
-
+       
        //phuong thuc
        public void NhapThongTin()
        {
