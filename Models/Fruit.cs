f3d2478dac7fd379d1218ebdb4a43d91152e99ee
SyTien270302 <2021050637@student.humg.edu.vn>
2023-10-03 22:57:53 +0700

    Xu ly ngoai le try...catch

--- Models/Fruit.cs
+++ Models/Fruit.cs
@@ -1,18 +1,33 @@
-using NewApp.Models;
-public class Fruit
-// nguyễn sỹ tiến - 2021050637
+namespace NewApp.Models
 {
-    private static void main(string[] args)
+    public class Fruit
     {
-        //khoitaodoituong
-        Fruit fr1 = new Fruit();
-        Fruit fr2 = new Fruit();
-        //gangiatrithuoctinh
-        Fruit1.Name = "Apple";
-        Fruit2.NguonGoc = "Korea";
-        //goi pt hien thi thong tin
-        fr1.Display();
-        fr2.Display();
+        public string TenLoaiQua { get; set; }
+        public int GiaSanPham { get; set; }
+        public int KhoiLuong { get; set; }
+        public void NhapThongTin()
+        //nguyễn sỹ tiến - 2021050637
+        {
+            
+           System.Console.Write("Ten Loai Qua = ");
+           TenLoaiQua = Console.ReadLine();
+           System.Console.Write("Gia San Pham = ");
+           GiaSanPham = Convert.ToInt32(Console.ReadLine());
+           System.Console.Write("Khoi Luong = ");
+           KhoiLuong = Convert.ToInt32(Console.ReadLine());
+           //try ... catch
+           try{
+           System.Console.Write("Gia San Pham = ");
+           GiaSanPham = Convert.ToInt32(Console.ReadLine());
+           }catch(Exception e)
+           {
+            System.Console.WriteLine("Co ngoai le xay ra");
+           }
+        }
+        public void HienThi()
+        {
+            System.Console.WriteLine("{0} - {1} - {2} kg", TenLoaiQua, GiaSanPham, KhoiLuong);
+        }
     }
 }
      
