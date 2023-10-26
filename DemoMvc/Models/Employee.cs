b73bf34e6255958b20db332963ce9b2a958d2070
SyTien270302 <2021050637@student.humg.edu.vn>
2023-10-26 22:39:33 +0700

    Employee MVC

---
+++ DemoMvc/Models/Employee.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models
+{
+    public class Employee 
+    {
+        // Nguyễn Sỹ Tiến - 2021050637
+        public string FullName { get; set; }
+        public int HeSoLuong { get; set; } 
+        public int LuongCB { get; set; }
+        public int PhuCap { get; set; }
+   
+    }
+}
