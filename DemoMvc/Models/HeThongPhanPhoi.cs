9a32903d5cf61ed60d9d3e958160389f3d19a4ab
SyTien270302 <2021050637@student.humg.edu.vn>
2023-10-28 22:46:02 +0700

    HTPP MVC

--- DemoMvc/Models/HeThongPhanPhoi.cs
+++ DemoMvc/Models/HeThongPhanPhoi.cs
@@ -1,7 +1,7 @@
 namespace FirstWebMVC.Models
 // Nguyễn Sỹ Tiến - 2021050637
 {
-    public class HeThongPhanPhoi 
+    public class HeThongPhanPhoi
     {
         public string MaHTPP { get; set; }
         public string TenHTPP { get; set; }
