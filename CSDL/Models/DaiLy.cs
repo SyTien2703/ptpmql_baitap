184a7bfb93699daa2de986d6a6036ed100c9804b
SyTien270302 <2021050637@student.humg.edu.vn>
2023-11-11 22:47:36 +0700

    CRUD

---
+++ CSDL/Models/DaiLy.cs
@@ -0,0 +1,11 @@
+namespace CSDL.Models
+{
+    public class DaiLy : HeThongPhanPhoi
+    {
+        public required string MaDaiLy { get; set; }
+        public required string TenDaiLy { get; set; }
+        public required string DiaChi { get; set; }
+        public required string NguoiDaiDien { get; set; }
+        public required string DienThoai { get; set; }
+    }
+}
