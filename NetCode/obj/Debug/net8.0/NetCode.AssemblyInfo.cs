16d22b26b76d781b55506814b8d7c903f796cc39
SyTien270302 <2021050637@student.humg.edu.vn>
2023-12-14 23:26:51 +0700

    Faculty

--- NetCode/obj/Debug/net8.0/NetCode.AssemblyInfo.cs
+++ NetCode/obj/Debug/net8.0/NetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce29b2a19a9974c88c2cb55f617e36811cb3d921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
